0d38adb921dbd917bb7c48d683177d11945639f3
bokoffalexander <bokoffalexander@gmail.com>
2024-07-12 14:08:37 +0300

    path and query

--- myapi.py
+++ myapi.py
@@ -14,6 +14,15 @@ students = {
 def index():
     return {"name": "First Data"}
 
+# path parameter
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path( description="Введите id студента", gt=0, lt=3)):
     return students[student_id]
+
+# query parameter
+@app.get("/get-by-name")
+def get_student(name: str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not Found"}
